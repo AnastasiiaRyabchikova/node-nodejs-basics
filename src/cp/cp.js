19b149b4cc80ad0230a4382af566703b82e1b133
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2022-12-05 23:32:53 +0300

    src/cp/cp.js: create

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,11 @@
+import child_process from 'node:child_process';
+import process from 'node:process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const cp = child_process.spawn('node', ['./src/cp/files/script.js', ...args]);
+
+    process.stdin.pipe(cp.stdin) 
+    cp.stdout.pipe(process.stdout)
 };
 
-spawnChildProcess();
+spawnChildProcess(['--arg1', '--arg2']);
