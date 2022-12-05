1413a3ce89b2b7793c613398829fc98ff621bf11
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2022-12-05 18:59:16 +0300

    src/zip/decompress.js: create

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs';
+import { unzip } from 'node:zlib';
+import { promisify } from 'node:util';
+
+const do_unzip = promisify(unzip);
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream('./src/zip/files/archive.gz');
+    const writeStream = fs.createWriteStream('./src/zip/files/fileToCompress.txt');
+
+    readStream.on('data', async (buffer) => {
+        const str = await do_unzip(buffer);
+        writeStream.write(str);
+    });
 };
 
 await decompress();
