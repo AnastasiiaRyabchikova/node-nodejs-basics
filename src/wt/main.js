71fd53e39c554d057c2a8a12c9e80322a94e1f2f
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2022-12-05 22:27:01 +0300

    src/wt: end

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,18 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpus = os.cpus();
+    const data = await Promise.allSettled(cpus.map((_, index) => (
+        new Promise((resolve, reject) => {
+            const worker = new Worker('./src/wt/worker.js', {
+              workerData: index + 10
+            });
+            worker.on('message', (data) => resolve({ status: 'resolved', data }));
+            worker.on('error', () => reject({ status: 'error', data: null }));
+          })
+    )));
+    console.log(data);
 };
 
 await performCalculations();
